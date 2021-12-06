35e0b133da00852cb5fe7bab1fa8a479f7f3b3c3
Fabienne RONDI <f.rondi@orange.fr>
2021-12-06 08:46:31 +0100

    flake8 corrections

--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 
-# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum,
+# Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam
+# lobortis leo consectetur ullamcorper non id est. Praesent dictum,
 # nulla eget feugiat sagittis, sem mi convallis eros,
 # vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget
 # consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
